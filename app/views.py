d0d20f8599cf329935abfa743962cc08899395ed
alex <kurochkinalexei@yandex.ru>
2023-05-31 11:19:32 +0300

    -

--- app/views.py
+++ app/views.py
@@ -1,26 +1,47 @@
 from django.core.mail import send_mail
 from django.shortcuts import render, redirect
+
+from portfolio import settings
 from .models import *
 
 
-def feedback(request):
+# def feedback(request):
+#     if request.method == "POST":
+#         msg = request.POST.get('msg')
+#         print(msg)
+#         project = request.POST.get('project')
+#         print(project)
+#         email = request.POST.get('email')
+#         # if email:
+#         #     msg += f'\nОтправитель: {email}'
+#         mail = send_mail(
+#             'Фитбэк с сайта-визитки',
+#             # msg + project + email,
+#             msg,
+#             settings.EMAIL_HOST_USER,
+#             ['KurochkinAlexei@yandex.ru',],
+#             # fail_silently=False
+#         )
+#     return redirect('index')
+
+
+def index(request):
     if request.method == "POST":
         msg = request.POST.get('msg')
         project = request.POST.get('project')
         email = request.POST.get('email')
         if email:
             msg += f'\nОтправитель: {email}'
-        mail = send_mail(
-            'Фитбэк с сайта-визитки',
+        send_mail(
+            f'Фитбэк на проект {project}',
+            # f'Сообщение: {msg} \nОтправитель: {email}',
             msg,
             settings.EMAIL_HOST_USER,
             ['KurochkinAlexei@yandex.ru'],
             fail_silently=False
         )
-    return redirect('index')
 
-
-def index(request):
+    # if request.method == "GET":
     skills = Skill.objects.all()
 
     for s in skills:
